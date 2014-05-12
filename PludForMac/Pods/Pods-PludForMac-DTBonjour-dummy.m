ef3d406eef5fa6860ecb323e3a00b59790d09dd8
iTSangar <italosangar@gmail.com>
2014-05-12 13:02:31 -0300

    Add Podfile for DTBonjour
    https://github.com/Cocoanetics/DTBonjour

---
+++ PludForMac/Pods/Pods-PludForMac-DTBonjour-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PludForMac_DTBonjour : NSObject
+@end
+@implementation PodsDummy_Pods_PludForMac_DTBonjour
+@end
---
+++ PludForMac/Pods/Pods-PludForMac-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PludForMac : NSObject
+@end
+@implementation PodsDummy_Pods_PludForMac
+@end
