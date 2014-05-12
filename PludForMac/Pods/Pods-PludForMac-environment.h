a02c284e8ea61b97ce5084e7e9d3de0535cca3f5
iTSangar <italosangar@gmail.com>
2014-05-12 17:44:05 -0300

    Add podfile for AXStatusItemPopup
    https://github.com/aschuch/AXStatusItemPopup

--- PludForMac/Pods/Pods-PludForMac-environment.h
+++ PludForMac/Pods/Pods-PludForMac-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// AXStatusItemPopup
+#define COCOAPODS_POD_AVAILABLE_AXStatusItemPopup
+#define COCOAPODS_VERSION_MAJOR_AXStatusItemPopup 0
+#define COCOAPODS_VERSION_MINOR_AXStatusItemPopup 0
+#define COCOAPODS_VERSION_PATCH_AXStatusItemPopup 2
+
 // DTBonjour
 #define COCOAPODS_POD_AVAILABLE_DTBonjour
 #define COCOAPODS_VERSION_MAJOR_DTBonjour 1
